23f99df5ba09b4ff6f43000dd86b6193c4fd8942
Sean Sollestre <sean.sollestre@gmail.com>
2024-04-06 23:15:41 -0700

    Swap to json format

--- lib/hello_web/live/circle_live.ex
+++ lib/hello_web/live/circle_live.ex
@@ -2,11 +2,12 @@ defmodule HelloWeb.CircleLive do
   use HelloWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :circle_y, 100)}
+    initial_y = 100
+    json_y = %{y: initial_y}
+    {:ok, assign(socket, :circle_y, json_y)}
   end
 
-  def handle_event("move_up", value, socket) do
-    IO.inspect(value)
-    {:noreply, update(socket, :circle_y, &(&1 - 10))}
+  def handle_event("move_up", _value, socket) do
+    {:noreply, update(socket, :circle_y, fn %{y: current_y} -> %{y: current_y - 10} end)}
   end
 end
